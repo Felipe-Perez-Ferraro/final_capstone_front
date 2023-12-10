5f63146b54182c05721fe8bcdb544286500db725
Hajnalka Oltyan <hajnalka.oltyan@gmail.com>
2023-12-10 23:07:50 +0100

    Add basic navigation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,36 @@
-import React from 'react';
+import { useSelector } from 'react-redux';
+import { NavLink } from 'react-router-dom';
+import { selectUser } from '../redux/usersession/usersessionsSlice';
 import Logout from './Logout';
 
-const Header = () => (
-  <header>
-    <h1>Header</h1>
-    <Logout />
-  </header>
-);
+const Header = () => {
+  const user = useSelector(selectUser);
+  const links = [
+    { path: '/', text: 'Boats' },
+    { path: '/reservations', text: 'Reservations' },
+    ...(user ? [] : [{ path: '/login', text: 'Login' }, { path: '/signup', text: 'Signup' }]),
+  ];
+
+  return (
+    <header>
+      <nav>
+        <ul>
+          {links.map(({ path, text }) => (
+            <li key={text}>
+              <NavLink to={path}>
+                {text}
+              </NavLink>
+            </li>
+          ))}
+          {user && (
+            <li>
+              <Logout />
+            </li>
+          )}
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
 export default Header;
