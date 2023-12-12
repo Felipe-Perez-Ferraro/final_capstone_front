dd939ec275e0118e63b926ba7b1c08d8e3c1efe9
Felipe <fperezferraro@gmail.com>
2023-12-11 21:58:54 -0300

    Added reservation reducer to the store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import usersessionReducer from './usersession/usersessionsSlice';
+import reservationsReducer from './reservations/reservationsSlice';
 
 const store = configureStore({
   reducer: {
     usersession: usersessionReducer,
+    reservations: reservationsReducer,
   },
 });
 
