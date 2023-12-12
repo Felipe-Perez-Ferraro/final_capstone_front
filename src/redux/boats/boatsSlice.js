831229d435b9d9518f8d29068afb2c5f377fd6c4
Felipe <fperezferraro@gmail.com>
2023-12-12 17:13:02 -0300

    Created deleteBoat function

--- src/redux/boats/boatsSlice.js
+++ src/redux/boats/boatsSlice.js
@@ -10,10 +10,25 @@ const initialState = {
 export const fetchBoats = createAsyncThunk('boats/fetchBoats', async () => {
   try {
     const response = await axios.get('http://localhost:3001/api/v1/boats');
-    console.log(response.data.data);
     return response.data.data;
   } catch (error) {
     return error.response.data;
+  }
+});
+
+export const deleteBoat = createAsyncThunk('boat/deleteboat', async (id) => {
+  try {
+    const response = await fetch(`http://127.0.0.1:3001/api/v1/boats/${id}`, {
+      method: 'DELETE',
+    });
+
+    if (response.ok) {
+      return { id };
+    }
+    const errorData = await response.json();
+    throw new Error(errorData.message || 'Failed to delete post');
+  } catch (error) {
+    throw new Error(error.message || 'Failed to delete boat');
   }
 });
 
@@ -33,6 +48,10 @@ const boatsSlice = createSlice({
       .addCase(fetchBoats.rejected, (state, action) => {
         state.isLoading = false;
         state.errors = action.payload;
+      })
+      .addCase(deleteBoat.fulfilled, (state, action) => {
+        const boatId = action.payload.id;
+        state.boats = state.boats.filter((boat) => boat.id !== boatId);
       });
   },
 });
