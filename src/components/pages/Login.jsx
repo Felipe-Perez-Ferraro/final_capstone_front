c0a40a2abedb4e53bb777e1a70fbddba1987fc24
Hajnalka Oltyan <hajnalka.oltyan@gmail.com>
2023-12-16 13:56:30 +0100

    Add alert messages for Login, when the name field empty, succesful and unsuccesful login

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,26 +1,39 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom';
+import Swal from 'sweetalert2';
 import { getUserLogin } from '../../redux/usersession/usersessionsSlice';
 
 const Login = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const [name, setName] = useState('');
-  const [formError, setFormError] = useState(null);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
     if (!name) {
-      setFormError('Name field cannot be empty');
+      Swal.fire({
+        title: 'Form Error',
+        text: 'Name field cannot be empty',
+        icon: 'error',
+      });
       return;
     }
     try {
       await dispatch(getUserLogin({ name })).unwrap();
       setName('');
+      Swal.fire({
+        title: 'Login Successful',
+        text: 'You have successfully logged in',
+        icon: 'success',
+      });
       navigate('/boats');
     } catch (err) {
-      console.error('Failed to login: ', err);
+      Swal.fire({
+        title: 'Failed to login',
+        text: 'User is not registered, or the name is incorrect',
+        icon: 'error',
+      });
     }
   };
 
@@ -39,7 +52,6 @@ const Login = () => {
             className="border-2 border-black rounded-md ml-2 w-[70%]"
           />
         </label>
-        {formError && <p className="text-red-500">{formError}</p>}
         <button type="submit" className="border-2 border-lime-400 bg-lime-400 rounded px-4 py-2 w-32 self-center my-10">Login</button>
       </form>
 
